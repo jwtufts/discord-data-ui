1fa6cfa255f801420c18cf892621414d9992eb8b
Jared Tufts <jaredtufts7@gmail.com>
2025-08-21 19:08:21 -0400

    initial testing login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <a
+      href={
+        "https://discord.com/oauth2/authorize?client_id=1407178750441033798&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Flogin%2Foauth2%2Fcode%2Fdiscord&scope=email+identify"
+      }
+    >
+      Login
+    </a>
+  );
 }
 
 export default App;
